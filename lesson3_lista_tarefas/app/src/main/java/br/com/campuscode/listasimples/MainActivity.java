738d2348ec6ded5133fef33968cd2447ff7deaec
Lucas Ferreira <lucasfe@gmail.com>
2017-08-05 12:36:28 -0300

    Criando uma lista de tarefas
    Signed-off-by: Lucas Ferreira <lucasfe@gmail.com>

---
+++ lesson3_lista_tarefas/app/src/main/java/br/com/campuscode/listasimples/MainActivity.java
@@ -0,0 +1,19 @@
+package br.com.campuscode.listasimples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //TODO vamos localizar a nossa lista no arquivo de layout
+        //TODO Crie um array de strings com as tasks que estarao na nossa lista
+        //TODO Crie um array adapter para nossa lista
+        //TODO na nossa listview vamos linkar nosso adapter com o metodo setAdapter
+
+    }
+}
